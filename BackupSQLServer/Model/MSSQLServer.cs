0b2cd9dfadb33e441bfb9f14c1401232a55f9bef
Everaldo Almeida <everaldo.maestro@gmail.com>
2017-07-16 13:16:02 -0300

    .

---
+++ BackupSQLServer/Model/MSSQLServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace Model
+{
+    public class MSSQLServer
+    {
+        public void BackupSQL()
+        {
+            Server server = new Server("TI");
+            Backup backup = new Backup()
+            {
+                Database = "NORTHWND",
+                Incremental = false
+            };
+            string nomeArquivoBackup = string.Format("{0}_{1:yyyyMMdd_HHmmss}.bak", "NORTHWND", DateTime.Now);
+            backup.Devices.AddDevice(nomeArquivoBackup, DeviceType.File);
+            backup.SqlBackup(server);
+        }
+    }
+}
---
+++ BackupSQLServer/View/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Model.MSSQLServer ms = new Model.MSSQLServer();
+            ms.BackupSQL();
+        }
+    }
+}
